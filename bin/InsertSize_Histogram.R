858a8cdd6e6ebf2f58b9afacb2f242ea16418c00
Rohit Satyam <rohitsatyam102@gmail.com>
2026-04-06 18:23:07 +0300

    Update InsertSize_Histogram.R
    Improved plotting and made xlim flexible, removed bounds as well.

--- bin/InsertSize_Histogram.R
+++ bin/InsertSize_Histogram.R
@@ -4,128 +4,90 @@
 library(RColorBrewer)
 
 ######## DATA LOADING ###########
-args   <- commandArgs(TRUE)
-minSize<-args[1]
-maxSize<-args[2]
+args <- commandArgs(TRUE)
+minSize <- as.numeric(args[1])
+maxSize <- as.numeric(args[2])
 
-#defining color palette
-dark2 <- c(RColorBrewer::brewer.pal(8, "Dark2"),RColorBrewer::brewer.pal(8, "Set1"),RColorBrewer::brewer.pal(8, "Set2"))
+# defining color palette
+dark2 <- c(
+  brewer.pal(8, "Dark2"),
+  brewer.pal(8, "Set1"),
+  brewer.pal(8, "Set2")
+)
 
-files<-list.files(path="." ,pattern = "insert_size_histogram.txt", full.names = TRUE, recursive = TRUE)
+files <- list.files(
+  path = ".",
+  pattern = "insert_size_histogram.txt",
+  full.names = TRUE,
+  recursive = TRUE
+)
 
-#sample names
-samples<-strsplit(files, split = "/")
-names<-NULL
-for(i in 1:length(samples)){
-names<-c(names,samples[[i]][2])
+# sample names
+samples <- strsplit(files, split = "/")
+names <- sapply(samples, function(x) x[2])
+
+# import data
+data <- lapply(files, read.table)
+
+# normalize data
+for (i in seq_along(data)) {
+  data[[i]]$V3 <- data[[i]]$V2 / sum(data[[i]]$V2)
 }
 
+# global x/y limits
+x_all <- unlist(lapply(data, function(d) d$V1))
+xlim_all <- c(min(x_all) - 50, max(x_all) + 50)
 
-#import data
-data<-lapply(files, read.table)
+high <- max(sapply(data, function(d) max(d$V2)))
+high_fr <- max(sapply(data, function(d) max(d$V3)))
 
-#normalize data
-for(i in 1:length(data)){
-  data[[i]]$V3<-data[[i]]$V2/sum(data[[i]]$V2)
-}
-
-
-#find max value for ylim 
-for(i in 1:length(data)){
-  curr<-max(data[[i]]$V2)
-  if(i==1){
-    high<-curr
-  }
-  else{
-    if(curr>high){
-      high<-curr
+# helper function to draw a plot with legend outside
+plot_insert_hist <- function(ycol, ylim, ylab, outfile) {
+  pdf(outfile, width = 10, height = 5)
+  
+  # reserve right margin for legend
+  par(mar = c(6.1, 5.1, 4.1, 12.1), xpd = FALSE)
+  
+  for (i in seq_along(data)) {
+    if (i == 1) {
+      plot(
+        data[[i]]$V1, data[[i]][[ycol]],
+        type = "l",
+        col = dark2[i],
+        lwd = 2,
+        xlim = xlim_all,
+        ylim = c(0, ylim),
+        xlab = "Insert size [bp]",
+        ylab = ylab,bty = "l"
+      )
+    } else {
+      lines(
+        data[[i]]$V1, data[[i]][[ycol]],
+        col = dark2[i],
+        lwd = 2
+      )
     }
   }
+  par(xpd = FALSE)
+  abline(v = c(minSize, maxSize), col = "black", lty = 2)
+  
+  # place legend in the right margin, outside plot area
+  legend(
+    "topright",
+    inset = c(-0.28, 0),
+    legend = names,
+    col = dark2[seq_along(data)],
+    lty = 1,
+    lwd = 2,
+    bty = "n",
+    cex = 0.8,
+    y.intersp = 1, xpd = NA
+  )
+  
+  dev.off()
 }
 
-
-#find max value for ylim fraction
-for(i in 1:length(data)){
-  curr_fr<-max(data[[i]]$V3)
-  if(i==1){
-    high_fr<-curr_fr
-  }
-  else{
-    if(curr_fr>high_fr){
-      high_fr<-curr_fr
-    }
-  }
-}
-
-
-
-## show InsertSizeHistogram
-
-pdf("InsertSizeHistogram_wide.pdf",
-        width = 8, height = 5)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-for(i in 1:length(data)){
-  if(i==1){
-    plot(data[[i]]$V1,data[[i]]$V2, type="l", col=dark2[i], ylim=c(0,high), xlim=c(55,500), ylab="Counts",xlab="Insert size [bp]", lwd=2)
-  }
-  else{
-    lines(data[[i]]$V1,data[[i]]$V2, type="l", col=dark2[i],lwd=2)
-  }
-}
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=FALSE)
-abline(v=c(minSize,maxSize), col="black", lty=2)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-legend(550,1.1*high,legend=names, col=dark2[1:length(data)],lty = 1, cex = 0.8, bty="n", lwd=2)
-dev.off()
-
-pdf("InsertSizeHistogram_Fraction_wide.pdf",
-    width = 8, height = 5)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-for(i in 1:length(data)){
-  if(i==1){
-    plot(data[[i]]$V1,data[[i]]$V3, type="l", col=dark2[i], ylim=c(0,high_fr),xlim=c(55,500), ylab="Fraction",xlab="Insert size [bp]",lwd=2)
-  }
-  else{
-    lines(data[[i]]$V1,data[[i]]$V3, type="l", col=dark2[i], lwd=2)
-  }
-}
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=FALSE)
-abline(v=c(minSize,maxSize), col="black", lty=2)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-legend(550,1.1*high_fr,legend=names, col=dark2[1:length(data)],lty = 1, cex = 1, bty="n", lwd=2)
-dev.off()
-
-
-pdf("InsertSizeHistogram_narrow.pdf",
-        width = 8, height = 5)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-for(i in 1:length(data)){
-  if(i==1){
-    plot(data[[i]]$V1,data[[i]]$V2, type="l", col=dark2[i], ylim=c(0,high), xlim=c(0,200), ylab="Counts",xlab="Insert size [bp]", lwd=2)
-  }
-  else{
-    lines(data[[i]]$V1,data[[i]]$V2, type="l", col=dark2[i],lwd=2)
-  }
-}
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=FALSE)
-abline(v=c(minSize,maxSize), col="black", lty=2)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-legend(230,1.1*high,legend=names, col=dark2[1:length(data)],lty = 1, cex = 0.8, bty="n", lwd=2)
-dev.off()
-
-pdf("InsertSizeHistogram_Fraction_narrow.pdf",
-    width = 8, height = 5)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-for(i in 1:length(data)){
-  if(i==1){
-    plot(data[[i]]$V1,data[[i]]$V3, type="l", col=dark2[i], ylim=c(0,high_fr),xlim=c(0,200), ylab="Fraction",xlab="Insert size [bp]",lwd=2)
-  }
-  else{
-    lines(data[[i]]$V1,data[[i]]$V3, type="l", col=dark2[i], lwd=2)
-  }
-}
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=FALSE)
-abline(v=c(minSize,maxSize), col="black", lty=2)
-par(mar=c(6.1, 5.1, 5.1, 18.1),xpd=TRUE)
-legend(230,1.1*high_fr,legend=names, col=dark2[1:length(data)],lty = 1, cex = 1, bty="n", lwd=2)
-dev.off()
+plot_insert_hist("V2", high, "Counts",   "InsertSizeHistogram_wide.pdf")
+plot_insert_hist("V3", high_fr, "Fraction", "InsertSizeHistogram_Fraction_wide.pdf")
+plot_insert_hist("V2", high, "Counts",   "InsertSizeHistogram_narrow.pdf")
+plot_insert_hist("V3", high_fr, "Fraction", "InsertSizeHistogram_Fraction_narrow.pdf")
